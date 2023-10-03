600262c02db5f1781831fadaa72a742b18c15e28
Blvcko13 <111675642+Blvcko13@users.noreply.github.com>
2023-10-03 19:34:53 +0200

    modified:   src/AboutMe.js 	modified:   src/App.css

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,9 +7,11 @@ function AboutMe(){
 
     return (
         <div >
-            <h1 className='header'> Hi &#128075; I am <br/> Jakhonkulov Javokhirjon</h1>
-            <h2 className='header'>I'm front end developer</h2>
-            <Button variant='outline-success'>Download CV</Button><br/>
+            <h1 className='header'> &#128075;Hi I am <br/> Jakhonkulov Javokhirjon</h1>
+            <h2 className='header'>Frontend developer</h2>
+            <Button variant='outline-success'> 
+            <a href='https://drive.google.com/u/0/uc?id=16PSNmveYEhe0BuQUd6q4n-4Mu1fedQxq&export=download' className='downloadlink'>Download CV</a>
+            </Button><br/>
             <img src={webdev} alt='Webdeveloper' /><br/>
         </div>
     );
