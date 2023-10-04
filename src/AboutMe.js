4a9897759b3a5e1a8a101a7a3bc0bd7b9e2039f5
Blvcko13 <111675642+Blvcko13@users.noreply.github.com>
2023-10-04 04:31:07 +0200

    modified:   src/AboutMe.js

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe(){
             <h1 className='header'> &#128075;Hi I am <br/> Jakhonkulov Javokhirjon</h1>
             <h2 className='header'>Frontend developer</h2>
             <Button variant='outline-success'> 
-            <a href='https://drive.google.com/u/0/uc?id=16PSNmveYEhe0BuQUd6q4n-4Mu1fedQxq&export=download' className='downloadlink'>Download CV</a>
+            <a href='https://drive.usercontent.google.com/download?id=1j_YToxrkPnMODtfu2N_hBnKmjlDGvUqu&export=download&authuser=0&confirm=t&uuid=1cbd04d0-bbe8-4ea4-848f-b5693e4534c8&at=APZUnTW6JKk-1QAypNjEa884x3Ek:1696386484889' className='downloadlink'>Download CV</a>
             </Button><br/>
             <img src={webdev} alt='Webdeveloper' /><br/>
         </div>
