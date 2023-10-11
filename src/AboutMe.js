0e08ea679c9f534a634bf8630fb36337f306ce98
Blvcko13 <111675642+Blvcko13@users.noreply.github.com>
2023-10-11 20:47:42 +0200

    cv link update

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe(){
             <h1 className='header'> &#128075;Hi I am <br/> Jakhonkulov Javokhirjon</h1>
             <h2 className='header'>Frontend developer</h2>
             <Button variant='outline-success'> 
-            <a href='https://drive.usercontent.google.com/download?id=1litqMvy8sZRWCaeK6ZNhbG2FazFLxpk4&export=download&authuser=0&confirm=t&uuid=1b9eba08-6acd-408c-a321-188556cc9f9d&at=APZUnTUtQZh6qlgGnlXQXGQDt4-1:1697043512928' className='downloadlink'>Download CV</a>
+            <a href='https://drive.usercontent.google.com/download?id=15i6yB8VFByOEXfXs8SdyCZX4rU4NfDpa&export=download&authuser=0&confirm=t&uuid=73b79abe-7e1e-4640-8faa-27e59e4623c2&at=APZUnTU2rRf2arZ01GOZDz4I51z_:1697049951992' className='downloadlink'>Download CV</a>
             </Button><br/>
             <img src={webdev} width="75%" alt='Webdeveloper' /><br/>
         </div>
